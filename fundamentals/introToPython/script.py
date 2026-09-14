91ff33c79828ab56aec554b54bf689089f35bf6b
Alexis Barraza-Andrade <149026419+abarraza24@users.noreply.github.com>
2026-09-14 17:00:18 -0600

    Completed Challange

---
+++ fundamentals/introToPython/script.py
@@ -0,0 +1,23 @@
+# Challange: Try out the code!
+
+# 1. Change the program's variables so that the invitation prints your name and today's date
+# 2. Change the dress code to "Casual." No need to dress up to learn Python!
+# 3. Run the Python file to check your work
+
+# Variables
+host = "Scrimba"
+date = "September 14"
+dress_code = "Casual"
+name = "Alex"
+
+print("=" * 40)
+print()
+print(f"  {name}, you are cordially invited to:")
+print()
+print("\tLEARN PYTHON")
+print()
+print(f"  Hosted by: {host}")
+print(f"  Date: {date}")
+print(f"  Dress code: {dress_code}")
+print()
+print("=" * 40)
